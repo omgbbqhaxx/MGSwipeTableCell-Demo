f9b8bf835c7f317f551f2a7d941c2a1a6ac8ed7a
yasin aktimur <yasinaktimur@yasin-MacBook-Air.local>
2015-08-11 17:50:08 +0300

    Initial Commit

---
+++ omg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  omg
+//
+//  Created by yasin aktimur on 11.08.2015.
+//  Copyright (c) 2015 yasin aktimur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ omg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  omg
+//
+//  Created by yasin aktimur on 11.08.2015.
+//  Copyright (c) 2015 yasin aktimur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
